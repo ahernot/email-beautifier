8e6265fc787dbd003e3e51e2ede46831377521c2
ahernot <70817470+ahernot@users.noreply.github.com>
2020-10-17 15:51:05 +0200

    Structural updates

--- MAIN.py
+++ MAIN.py
@@ -13,69 +13,67 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 _psep_ = '/'
 _esep_ = '.'
 
-default_attributes = {
-    'text_color': '#000000',
-    'background_color': '#FFFFFF',
-    'style_underlined': False,
-    'style_strikethrough': False,
-    'style_italicised': False,
-    'style_bold': False,
-    'font_size': 11,
-    'style_font': """'arial black', 'avant garde'"""
-}
+
 
 beacon_chars_list = ['*']
 
 
 class Char(object):
 
+    default_attributes = {
+        'text_color': '#000000',
+        'background_color': '#FFFFFF',
+        'style_underlined': False,
+        'style_strikethrough': False,
+        'style_italicised': False,
+        'style_bold': False,
+        'font_size': 11,
+        'style_font': """'arial black', 'avant garde'"""
+    }
+
     def __init__(self, text:str, **kwargs):
 
         self.text = text
 
         #   Text color (CSS)
-        self.text_color = default_attributes['text_color']
+        self.text_color = default_attributes_char['text_color']
         if 'text_color' in kwargs:
             self.text_color = kwargs['text_color']
 
         #   Background color (CSS)
-        self.background_color = default_attributes['background_color']
+        self.background_color = default_attributes_char['background_color']
         if 'background_color' in kwargs:
             self.background_color = kwargs['background_color']
 
         #   Style underlined (CSS)
-        self.style_underlined = default_attributes['style_underlined']
+        self.style_underlined = default_attributes_char['style_underlined']
         if 'style_underlined' in kwargs:
             self.style_underlined = kwargs['style_underlined']
 
         #   Style strikethrough (CSS)
-        self.style_strikethrough = default_attributes['style_strikethrough']
+        self.style_strikethrough = default_attributes_char['style_strikethrough']
         if 'style_strikethrough' in kwargs:
             self.style_strikethrough = kwargs['style_strikethrough']
 
         #   Style italicised (HTML)
-        self.style_italicised = default_attributes['style_italicised']
+        self.style_italicised = default_attributes_char['style_italicised']
         if 'style_italicised' in kwargs:
             self.style_italicised = kwargs['style_italicised']
 
         #   Style bold (HTML)
-        self.style_bold = default_attributes['style_bold']
+        self.style_bold = default_attributes_char['style_bold']
         if 'style_bold' in kwargs:
             self.style_bold = kwargs['style_bold']
 
         #   Font size (CSS)
-        self.font_size = default_attributes['font_size']
+        self.font_size = default_attributes_char['font_size']
         if 'font_size' in kwargs:
             self.font_size = kwargs['font_size']
 
         #   Style font (CSS)
-        self.style_font = default_attributes['style_font']
+        self.style_font = default_attributes_char['style_font']
         if 'style_font' in kwargs:
             self.style_font = kwargs['style_font']
-
-
-
-
 
     def generate_html(self) -> str:
 
@@ -84,20 +82,19 @@ class Char(object):
         style_line_list = []
 
         #   2. Adding the CSS styles
-        style_line_list.append('color: {};'.format(self.text_color)) # text color
-        style_line_list.append('background-color: {};'.format(self.background_color)) # background color
-        if self.style_underlined: style_line_list.append('text-decoration: underline;') # underlined
-        if self.style_strikethrough: style_line_list.append('text-decoration: line-through;') # struckthrough
-        style_line_list.append('font-size: {}pt;'.format(self.font_size)) # font size
-        style_line_list.append('font-family: {};') # font style
+        style_line_list.append('color: {};'.format(self.text_color))  # text color
+        style_line_list.append('background-color: {};'.format(self.background_color))  # background color
+        if self.style_underlined: style_line_list.append('text-decoration: underline;')  # underlined
+        if self.style_strikethrough: style_line_list.append('text-decoration: line-through;')  # struckthrough
+        style_line_list.append('font-size: {}pt;'.format(self.font_size))  # font size
+        style_line_list.append('font-family: {};')  # font style
 
         #   3. Adding the HTML styles
         other_styles_list = []
-        if self.style_bold: other_styles_list.append('strong') # bold
-        if self.style_italicised: other_styles_list.append('em') # italicised
+        if self.style_bold: other_styles_list.append('strong')  # bold
+        if self.style_italicised: other_styles_list.append('em')  # italicised
         other_styles_start = ''.join(['<{}>'.format(style) for style in other_styles_list])
         other_styles_stop = ''.join(['</{}>'.format(style) for style in other_styles_list])
-
 
         #   4. Generating the line
         html_line = html_line.format(**{
@@ -112,6 +109,49 @@ class Char(object):
 
 
 
+    class Word(object):
+
+        def __init__(self, text:str, **kwargs):
+
+            self.text = text
+
+            # not the same as a char: all attributes are determined randomly
+
+
+        def stylise(self):
+
+            pass
+
+            # will either single out characters or choose gradients etc
+
+
+        def generate_html(self) -> str:
+            pass
+
+
+
+
+
+    class Line(object):
+
+        def __init__(self, text:str, **kwargs):
+
+            self.text = text
+
+            self.height = 0.7
+
+
+
+
+
+
+
+"""
+CHARACTER: span style="vertical-align: 5.0px;"
+WORD-WISE: span style="letter-spacing: -0.7px;"
+LINE-WISE: line-height: 0.7
+LINE-WISE: text-align: right/center/left/justify
+"""
 
 
 def random_style_char(text:str) -> Char:
@@ -124,20 +164,24 @@ def random_style_char(text:str) -> Char:
     background_color_hex = '#' + str(hex(background_color_int))[2:]
 
     #   Style underlined (CSS)
-    style_underlined = bool(random.randint(0, 1))
+    style_underlined_spread = [0, 0, 0, 0, 1, 1, 1]
+    style_underlined = bool(style_underlined_spread[random.randint(0, len(style_underlined_spread) - 1)]) #style_underlined = bool(random.randint(0, 1))
+
 
     #   Style strikethrough (CSS)
-    style_strikethrough = bool(random.randint(0, 1))
+    style_strikethrough_spread = [0, 0, 0, 0, 0, 0, 1]
+    style_strikethrough = bool(style_strikethrough_spread[random.randint(0, len(style_strikethrough_spread) - 1)]) #style_strikethrough = bool(random.randint(0, 1))
 
     #   Style italicised (HTML)
-    style_italicised = bool(random.randint(0, 1))
+    style_italicised_spread = [0, 0, 1, 1, 1]
+    style_italicised = bool(style_italicised_spread[random.randint(0, len(style_italicised_spread) - 1)]) #style_italicised = bool(random.randint(0, 1))
 
     #   Style bold (HTML)
-    style_bold = bool(random.randint(0, 1))
+    style_bold_spread = [0, 1, 1]
+    style_bold = bool(style_bold_spread[random.randint(0, len(style_bold_spread) - 1)]) #style_bold = bool(random.randint(0, 1))
 
     #   Font size (CSS)
-    #font_size = random.randint(6, 250)
-    font_size = numpy.abs(numpy.random.normal(50, 20, 1)[0])
+    font_size = numpy.abs(numpy.random.normal(50, 20, 1)[0]) #font_size = random.randint(6, 250)
 
     #   Style font (CSS)
     fonts = ['',
@@ -226,3 +270,6 @@ def convert(input_file_path: str):
 
 
 convert('/Users/Anatole/Desktop/t.txt')
+
+
+# <p style=""><span style="">text</span></p>
