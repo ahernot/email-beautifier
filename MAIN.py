7092c79be2471d11aab2e0a377c74ac1069930a3
ahernot <70817470+ahernot@users.noreply.github.com>
2020-10-17 11:19:47 +0200

    Create MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,32 @@
+
+
+
+text = '''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+'''
+
+# Every line starts with "<div>" and ends in "</div>"
+# Text color            <span style="color: #993300;">test</span>
+# Background color      <span style="background-color: #339966;">test</span>
+# Text & Bck color      <span style="color: #993300; background-color: #339966;">test</span>
+# Underline             <span style="text-decoration: underline;">test</span>
+# Italicize             <em>test</em>
+# Bold                  <strong>test</strong>
+# Strikethrough         <span style="text-decoration: line-through;">test</span>
+
+
+# Font size             <span style="font-size: 11pt;">test</span></div>
+# Font & size           <span style="font-size: 11pt;"><span style="font-family: 'arial black', 'avant garde';">Anatole</span> Hernot (P20)</span>
+
+
+
+'''
+random color (bck & txt)
+random style
+random font
+random size
+'''
+
+
+# if multiple same letters then choose between gradient / wave
+# color gradient, size gradient
